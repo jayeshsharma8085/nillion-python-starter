560d8cb3e2bed115e256e08405e9dcd50d5b5291
jayeshsharma8085 <jayeshsharma04035@gmail.com>
2024-06-25 15:23:53 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -3,10 +3,10 @@ from nada_dsl import *
 
 def nada_main():
     party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    radius = SecretInteger(Input(name="radius", party=party1))
+    
+    # Compute the area of the circle
+    pi = 3.141592653589793
+    area = pi * (radius ** 2)
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
-
-    return [Output(my_int1, "my_output", party1)]
+    return [Output(area, "area_output", party1)]
